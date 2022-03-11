1c1b382e648d77b51fe95002c9582d37dafa7a7d
mikhail.menko <menko.mihail@gmail.com>
2022-03-11 21:04:07 +0300

    Initial commit

---
+++ src/main/java/com/hesfintech/configclient/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.hesfintech.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${application.customer}")
+    private String customer;
+
+    @GetMapping(value = "/customer", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getCustomer() {
+        return String.format("Current customer is %s.", customer);
+    }
+}
